41c1fe18aefecf6aefd3adcfd4b6912be16df657
Deivid Sestren Santos <deividsestrensantos@gmail.com>
2026-08-26 19:31:43 -0300

    fix: ajuste ResponseDTO, deixando o mesmo imutavel e adicioanando boas práticas

--- src/main/java/me/deivid/crypto_price_alert/dto/UserAlertResponseDTO.java
+++ src/main/java/me/deivid/crypto_price_alert/dto/UserAlertResponseDTO.java
@@ -1,29 +1,27 @@
 package me.deivid.crypto_price_alert.dto;
 
-import lombok.Data;
 import me.deivid.crypto_price_alert.enums.AlertType;
 import me.deivid.crypto_price_alert.model.UserAlert;
 
 import java.math.BigDecimal;
 
-@Data
-public class UserAlertResponseDTO {
-
-        private Long id;
-        private String email;
-        private String symbol;
-        private BigDecimal priceLimit;
-        private AlertType alertType;
-        private boolean active;
-
+public record UserAlertResponseDTO(
+        Long id,
+        String email,
+        String symbol,
+        BigDecimal priceLimit,
+        AlertType alertType,
+        boolean active
+) {
         public static UserAlertResponseDTO from(UserAlert userAlert) {
-                UserAlertResponseDTO dto = new UserAlertResponseDTO();
-                dto.setId(userAlert.getId());
-                dto.setEmail(userAlert.getEmail());
-                dto.setSymbol(userAlert.getSymbol());
-                dto.setPriceLimit(userAlert.getPriceLimit());
-                dto.setAlertType(userAlert.getAlertType());
-                dto.setActive(userAlert.isActive());
-                return dto;
+
+                return new UserAlertResponseDTO(
+                        userAlert.getId(),
+                        userAlert.getEmail(),
+                        userAlert.getSymbol(),
+                        userAlert.getPriceLimit(),
+                        userAlert.getAlertType(),
+                        userAlert.isActive()
+                );
         }
 }
